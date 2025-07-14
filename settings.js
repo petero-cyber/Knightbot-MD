783e46be87d304a1408075b87aae717fbba9ef82
petero-cyber <peterkamau254727@gmail.com>
2025-07-14 12:46:52 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Ben Bot',
+  author: 'ben‎',
+  botName: "bEN Bot",
+  botOwner: 'Ben', // Your name
+  ownerNumber: '254727374449', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "pRIVATE",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
